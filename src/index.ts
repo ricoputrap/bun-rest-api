c4bbe9d6eb5d4a852bb94a050d63de482d3fe5f1
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-03-16 08:55:45 +0700

    Simple rest api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .state('version', 1)
+  .decorate('getDate', () => Date.now())
+  .get("/", () => "Hello Elysia")
+  .get("/posts", ({ store, getDate }) => ({
+    data: [
+      { id: 1, title: "Post 1" },
+      { id: 2, title: "Post 2" },
+      { id: 3, title: "Post 3" },
+    ],
+    version: store.version,
+    date: getDate()
+  }))
+  .get("/posts/:id", ({ params: { id } }) => { return { id } })
+  .post("/posts", ({ body, set }) => {
+    set.status = 201;
+    return body;
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
